d8c3cc67b2b6cdf4665b43dde90b44374413357a
Victor Idongesit <victoridongesit@gmail.com>
2018-03-03 22:27:56 +0100

    Initial Commit

---
+++ TopGitHubUsers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TopGitHubUsers
+//
+//  Created by Victor Idongesit on 03/03/2018.
+//  Copyright © 2018 Victor Idongesit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
